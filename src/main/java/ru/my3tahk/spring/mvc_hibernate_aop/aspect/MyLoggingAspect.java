f030e6a92f01bac4eacdac55bf3775bd14c7f5a7
t1nkt35t <t1nkt35t@gmail.com>
2021-09-07 14:50:32 +0300

    1st

---
+++ src/main/java/ru/my3tahk/spring/mvc_hibernate_aop/aspect/MyLoggingAspect.java
@@ -0,0 +1,21 @@
+package ru.my3tahk.spring.mvc_hibernate_aop.aspect;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.reflect.MethodSignature;
+import org.springframework.stereotype.Component;
+
+@Component
+@Aspect
+public class MyLoggingAspect {
+    @Around("execution(* ru.my3tahk.spring.mvc_hibernate_aop.dao.*.*(..))")
+    public Object aroundAllRepositoryMethodsAdvice(ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
+        MethodSignature methodSignature = (MethodSignature) proceedingJoinPoint.getSignature();
+        String methodName = methodSignature.getName();
+        System.out.println("method begins " + methodName);
+        Object targetMethodResult = proceedingJoinPoint.proceed();
+        System.out.println("method end " + methodName);
+        return targetMethodResult;
+    }
+}
---
+++ src/main/java/ru/my3tahk/spring/mvc_hibernate_aop/dao/EmployeeDAO.java
@@ -0,0 +1,15 @@
+package ru.my3tahk.spring.mvc_hibernate_aop.dao;
+
+import ru.my3tahk.spring.mvc_hibernate_aop.entity.Employee;
+
+import java.util.List;
+
+public interface EmployeeDAO {
+    public List<Employee> getAllEmployees();
+
+    public void saveEmployee(Employee employee);
+
+    public Employee getEmployee(int id);
+
+    public void deleteEmployee(int id);
+}
